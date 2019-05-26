53f1b8fad34c0fb62bc00bb2c08c2460e322bb5c
Mehrdad Shokri <code.poet95@gmail.com>
2019-05-26 17:00:11 +0430

    Adding intro page

--- app/app.js
+++ app/app.js
@@ -2,15 +2,15 @@ import React from 'react';
 import {Provider as StoreProvider} from 'react-redux';
 import {Provider as PaperProvider} from 'react-native-paper';
 import initialiseStore from './store/config/index';
-import App from './containers/App';
+import Intro from './containers/Intro';
+// import App from './containers/App';
 import theme from './utils/theme';
 const store = initialiseStore();
-
 
 export default () => (
     <StoreProvider store={store}>
         <PaperProvider theme={theme}>
-            <App/>
+            <Intro/>
         </PaperProvider>
     </StoreProvider>
 );
---
+++ app/containers/Intro/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native'
+// import AppIntroSlider from 'react-native-app-intro-slider';
+import App from '../App';
+
+const slides = [
+    {
+        key: 'somethun',
+        title: 'Title 1',
+        text: 'Description.\nSay something cool',
+        image: require('./assets/1.jpg'),
+        backgroundColor: '#59b2ab',
+    },
+    {
+        key: 'somethun-dos',
+        title: 'Title 2',
+        text: 'Other cool stuff',
+        image: require('./assets/2.jpg'),
+        backgroundColor: '#febe29',
+    },
+    {
+        key: 'somethun1',
+        title: 'Rocket guy',
+        text: 'I\'m already out of descriptions\n\nLorem ipsum bla bla bla',
+        image: require('./assets/3.jpg'),
+        backgroundColor: '#22bcb5',
+    }
+];
+
+class Intro extends React.Component {
+    state = {
+        showRealApp: false
+    };
+    _renderItem = (item) => {
+        return (
+            <View style={styles.slide}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Image source={item.image} />
+                <Text style={style.text}>{item.text}</Text>
+            </View>
+        );
+    };
+    _onDone = () => {
+        // User finished the introduction. Show real app through
+        // navigation or simply by controlling state
+        this.setState({ showRealApp: true });
+    };
+    render() {
+        return <App />;
+
+    }
+}
+
+export default Intro;
