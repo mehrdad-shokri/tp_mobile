c07a5dd853d352bac622e11ea2a4e368f885c290
Mehrdad Shokri <code.poet95@gmail.com>
2019-05-26 16:53:10 +0430

    Rename hofSaga to hof

--- app/store/config/rootSaga.js
+++ app/store/config/rootSaga.js
@@ -1,5 +1,5 @@
 import { fork, all } from 'redux-saga/dist/redux-saga-effects-npm-proxy.esm';
-import {hofSagas} from '../saga/hofSaga';
+import {hofSagas} from '../saga/hof';
 function* rootSaga() {
   yield all([
     fork(hofSagas)
